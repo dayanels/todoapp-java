59299998420683dcaa644ea703ae4fec088f4bb9
dayanels <lemos.dayane.d@gmail.com>
2023-10-15 11:02:14 -0300

    first commit

---
+++ src/main/java/lemos/dayane/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,69 @@
+package lemos.dayane.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lemos.dayane.todolist.user.IUserRepository;
+
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+                var serveltPath = request.getServletPath();
+
+                if(serveltPath.startsWith("/tasks/")){
+                    var authorization = request.getHeader("Authorization");
+
+                    var authEncoded = authorization.substring("Basic".length()).trim();
+
+                    byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+
+                    var authString = new String(authDecoded);
+
+                    String[] credentials = authString.split(":");
+                    String username = credentials[0];
+                    String password = credentials[1];
+                    var user = this.userRepository.findByUsername(username);
+
+                    if(user == null){
+                        response.sendError(401);
+                    }else{
+                        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+                        if(passwordVerify.verified){
+                            request.setAttribute("idUser", user.getId());
+                            filterChain.doFilter(request, response);
+                        }else{
+                            response.sendError(401);
+                        }
+                    }
+                }else{
+                    filterChain.doFilter(request, response);
+                }
+
+                
+
+                
+    }
+
+    
+}
